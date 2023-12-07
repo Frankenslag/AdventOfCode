7918a8f46e7cd77334170554a2dcab4470b31a83
Andrew Gould <andrew@wingandprayer.co.uk>
2023-12-07 15:43:52 +0000

    Day 5 2023

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -6,7 +6,9 @@
         {
             //Day1.Run();
             //Day2.Run();
-            Day3.Run();
+            //Day3.Run();
+            //Day4.Run();
+            Day5.Run();
         }
     }
 }
