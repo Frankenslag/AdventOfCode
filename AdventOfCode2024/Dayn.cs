7ff081a6b9bb41aba4bcdcfb35e5304737f4d622
Andrew Gould <andrew@wingandprayer.co.uk>
2024-12-01 12:18:50 +0000

    2024 Day 1

---
+++ AdventOfCode2024/Dayn.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AdventOfCode2024
+{
+    internal class Dayn
+    {
+        private static readonly string[] Data =
+        [
+        ];
+
+
+        private static readonly string[] Data1 =
+        [
+        ];
+
+        public static void Run()
+        {
+            Console.WriteLine($"Day 1 Part 1 Answer is calibration value {1}.");
+            Console.WriteLine($"Day 1 Part 2 Answer is calibration value {2}.");
+        }
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Day1.Run();
+        }
+    }
+}
