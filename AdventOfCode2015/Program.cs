00f3a30453795d86eef3d5e8cdf91cf0c64c4470
Andrew Gould <andrew@wingandprayer.co.uk>
2025-02-12 16:34:26 +0000

    2015 Day 18

--- AdventOfCode2015/Program.cs
+++ AdventOfCode2015/Program.cs
@@ -1,25 +1,27 @@
-﻿using AdventOfCode2015;
-
-class Program
+﻿namespace AdventOfCode2015
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Day1.Run();
-        Day2.Run();
-        Day3.Run();
-        Day4.Run();
-        Day5.Run();
-        Day6.Run();
-        Day7.Run();
-        Day8.Run();
-        Day9.Run();
-        Day10.Run();
-        Day11.Run();
-        Day12.Run();
-        Day13.Run();
-        Day14.Run();
-        Day15.Run();
-        Day16.Run();
-        Day17.Run();
+        private static void Main()
+        {
+            //Day1.Run();
+            //Day2.Run();
+            //Day3.Run();
+            //Day4.Run();
+            //Day5.Run();
+            //Day6.Run();
+            //Day7.Run();
+            //Day8.Run();
+            //Day9.Run();
+            //Day10.Run();
+            //Day11.Run();
+            //Day12.Run();
+            //Day13.Run();
+            //Day14.Run();
+            //Day15.Run();
+            //Day16.Run();
+            //Day17.Run();
+            Day18.Run();
+        }
     }
 }
