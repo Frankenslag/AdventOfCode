ecc975fe80a57d8131e157fe09134229a7c0ea08
Andrew Gould <andrew@wingandprayer.co.uk>
2025-09-15 13:55:49 +0100

    2023 Day7

--- AdventOfCode2023/Dayn.cs
+++ AdventOfCode2023/Dayn.cs
@@ -3,13 +3,18 @@ namespace AdventOfCode2023
 {
     internal class Dayn
     {
-        private static readonly string[] Data =
-        {
-        };
+        private static readonly string[] DataProd =
+        [
+        ];
 
+        private static readonly string[] DataTest =
+        [
+        ];
 
         public static void Run()
         {
+            string[] data = DataTest;
+
             Console.WriteLine($"Day 1 Part 1 Answer is calibration value {1}.");
             Console.WriteLine($"Day 1 Part 2 Answer is calibration value {2}.");
         }
