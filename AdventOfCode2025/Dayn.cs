ad065a20fc3dd94b1bdc36449fd528b57f86c1b5
Andrew Gould <andrew@wingandprayer.co.uk>
2025-12-02 14:38:38 +0000

    2025 Day 1

---
+++ AdventOfCode2025/Dayn.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AdventOfCode2025
+{
+    internal class Dayn
+    {
+        private static readonly string[] DataProd =
+        [
+        ];
+
+        private static readonly string[] DataTest =
+        [
+        ];
+
+        private static void Execute(string[] data)
+        {
+            Console.WriteLine($"Day n Part 1 Answer is calibration value {1}.");
+        }
+
+        public static void Run() => Execute(DataTest);
+    }
+}
